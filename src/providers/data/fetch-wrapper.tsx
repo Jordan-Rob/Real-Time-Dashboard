121fcd6e36f1b2e7906e196ad10ccba6e5b05ecc
Jordan Rob <jordanrob709@gmail.com>
2024-01-31 18:02:36 +0300

    formatted wrapper file

--- src/providers/data/fetch-wrapper.tsx
+++ src/providers/data/fetch-wrapper.tsx
@@ -1,58 +1,60 @@
-import { GraphQLFormattedError } from "graphql"
+import { GraphQLFormattedError } from "graphql";
 
 type Error = {
-    message: string,
-    statusCode: string
-}
+  message: string;
+  statusCode: string;
+};
 
 const customFetch = async (url: string, options: RequestInit) => {
-    const accessToken = localStorage.getItem('access_token')
+  const accessToken = localStorage.getItem("access_token");
 
-    const headers = options.headers as Record<string, string>
+  const headers = options.headers as Record<string, string>;
 
-    return await fetch(url, {
-        ...options,
-        headers: {
-            ...headers,
-            Authorization: headers?.Authorization || `Bearer ${accessToken}`,
-            "Content-Type": "application/json",
-            "Apollo-Require-Preflight": "true"
-        }
-    })
-}
+  return await fetch(url, {
+    ...options,
+    headers: {
+      ...headers,
+      Authorization: headers?.Authorization || `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+      "Apollo-Require-Preflight": "true",
+    },
+  });
+};
 
-const getGraphQLErrors = (body: Record<"errors", GraphQLFormattedError[] | undefined>): Error | null => {
-    if (!body) {
-        return {
-            message: 'Unknown error',
-            statusCode: "INTERNAL_SERVER_ERROR"
-        }
-    }
+const getGraphQLErrors = (
+  body: Record<"errors", GraphQLFormattedError[] | undefined>
+): Error | null | undefined => {
+  if (!body) {
+    return {
+      message: "Unknown error",
+      statusCode: "INTERNAL_SERVER_ERROR",
+    };
+  }
 
-    if("errors" in body){
-        const errors = body?.errors
+  if ("errors" in body) {
+    const errors = body?.errors;
 
-        const messages = errors?.map( error => error?.message)?.join("")
-        const code = errors?.[0]?.extensions?.code
+    const messages = errors?.map((error) => error?.message)?.join("");
+    const code = errors?.[0]?.extensions?.code;
 
-        return {
-            message: messages || JSON.stringify(messages),
-            statusCode: code || 500
-        }
-    }
-}
+    return {
+      message: messages || JSON.stringify(messages),
+      statusCode: code || 500,
+    };
+  }
+};
 
-export const fetchWrapper = async ( url: string, options: RequestInit) => {
-    const response = await customFetch(url, options)
+export const fetchWrapper = async (url: string, options: RequestInit) => {
+  const response = await customFetch(url, options);
 
-    const responseClone = response.clone()
-    const body = await responseClone.json()
+  const responseClone = response.clone();
+  const body = await responseClone.json();
 
-    const error = getGraphQLErrors(body)
+  const error = getGraphQLErrors(body);
 
-    if(error) {
-        throw error
-    }
+  if (error) {
+    throw error;
+  }
 
-    return response
-}
+  return response;
+};
