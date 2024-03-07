197220ad3cf01cd4231bfad822c4d12153974aca
Jordan Rob <jordanrob709@gmail.com>
2024-03-07 16:41:09 +0300

    worked on home/upcoming events card and skeleton ui

---
+++ src/components/home/deals-chart.tsx
@@ -0,0 +1,22 @@
+import { Card } from "antd";
+import React from "react";
+
+const DealsChart = () => {
+  return (
+    <Card
+      style={{
+        height: "100%",
+      }}
+      headStyle={{
+        padding: "8px 16px",
+      }}
+      bodyStyle={{
+        padding: "0 1rem",
+      }}
+    >
+      DealsChart
+    </Card>
+  );
+};
+
+export default DealsChart;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,41 @@
+import { Col, Row } from "antd";
+import { UpcomingEvents, DealsChart } from "@/components";
+
 export const Home = () => {
-    return (
-        <div>Home</div>
-    )
-}
+  return (
+    <div>
+      <Row gutter={[32, 32]} style={{ marginTop: "32px" }}>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px",
+          }}
+        >
+          <UpcomingEvents />
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px",
+          }}
+        >
+          <DealsChart />
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px",
+          }}
+        >
+          <UpcomingEvents />
+        </Col>
+      </Row>
+    </div>
+  );
+};
