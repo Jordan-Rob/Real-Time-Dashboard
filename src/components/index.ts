4a82a72ac560b7ced2a7a63e42d63f88e90519da
Jordan Rob <jordanrob709@gmail.com>
2024-03-12 19:31:00 +0300

    added upcoming events card to home

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,7 @@ import KanbanColumnSkeleton from "./skeleton/kanban";
 import ProjectCardSkeleton from "./skeleton/project-card";
 import LatestActivitiesSkeleton from "./skeleton/latest-activities";
 import DashboardTotalCountCard from "./home/total-count-card";
+import LatestActivities from "./home/latest-activities";
 
 export {
   UpcomingEvents,
@@ -15,5 +16,6 @@ export {
   KanbanColumnSkeleton,
   ProjectCardSkeleton,
   LatestActivitiesSkeleton,
-  DashboardTotalCountCard
+  DashboardTotalCountCard,
+  LatestActivities
 };
